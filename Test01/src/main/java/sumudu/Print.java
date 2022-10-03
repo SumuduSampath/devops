75a33c7f43f1a5aa91b8afea7e7541f95f709829
Sumudu Sampath <sumudus@healthreconconnect.com>
2022-10-04 01:00:17 +0530

    ini

---
+++ Test01/src/main/java/sumudu/Print.java
@@ -0,0 +1,13 @@
+package sumudu;
+
+import java.util.stream.Stream;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Stream.of("Records", "Pattern Matching", "Sealed Classess").filter(s -> s.contains(" ")).toList();
+	}
+
+}
---
+++ Test01/src/main/java/sumudu/Test001.java
@@ -0,0 +1,23 @@
+package sumudu;
+
+public class Test001 {
+	
+
+	public static void main(String[] args) {
+		Test001 test = new Test001();
+		test.testMethod("sumnudu",10);
+		
+		//we can't call non static method in side the static method .so we have to create class object to call relevant method.
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void testMethod(String name,int age) {
+	
+		//method should have a return type it can be String ,int or any data type or void type,when use void there is no return from particular method.
+		
+		
+	}
+
+}

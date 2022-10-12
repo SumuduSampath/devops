29e9f76a0250ad6100e7b32854a94e328db976d9
Sumudu Sampath <sumudus@healthreconconnect.com>
2022-10-12 21:18:47 +0530

    non

--- Test01/src/main/java/sumudu/Print.java
+++ Test01/src/main/java/sumudu/Print.java
@@ -7,7 +7,7 @@ public class Print {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Stream.of("Records", "Pattern Matching", "Sealed Classess").filter(s -> s.contains(" ")).toList();
+		//Stream.of("Records", "Pattern Matching", "Sealed Classess").filter(s -> s.contains(" ")).toList();
 	}
 
 }
